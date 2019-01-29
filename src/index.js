def2d75536b376fa493e5f6203c7eeef78558d65
Yadira-Lizeth <sainz_06@outlook.com>
2019-01-29 12:10:56 -0600

    Añadi otro HTML para abrir otra pestaña

---
+++ src/index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+
+function myFunction() {
+    //var n = parseInt(prompt ("introduce el desplzamiento"));
+    var resultado ="";
+    var n = parseInt(document.getElementById("numero").value);
+    //var texto = prompt ("introduce tu texto");
+    var texto = document.getElementById("contenido").value;
+    texto = String(texto).toUpperCase();
+    for (var x = 0; x < texto.length; x++) {
+        var code = (texto.charCodeAt(x) - 65 + n) % 26 + 65;
+        resultado = resultado+String.fromCharCode(code);
+    }
+    document.getElementById("resultado").value=resultado;
+}
