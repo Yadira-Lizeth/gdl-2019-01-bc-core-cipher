15b7204a6940542a3a4614b1dc04a99107f6b9e2
Yadira-Lizeth <sainz_06@outlook.com>
2019-01-31 16:12:52 -0600

    Modifeque de jv a cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,25 @@
 window.cipher = {
-  // ... 
+ 
+  encode:(offset, string) => {
+
+    var resultado ="";
+    texto = String(texto).toUpperCase();
+    for (var x = 0; x < texto.length; x++) {
+        var code = (texto.charCodeAt(x) - 65 + n) % 26 + 65;
+        resultado = resultado+String.fromCharCode(code);
+        
+    }
+    document.getElementById("resultado").value=resultado;
+},
+
+decode:(offset, string) => {
+    var resultado ="";
+    texto = String(texto).toUpperCase();
+    for (var x = 0; x < texto.length; x++) {
+        var code = (texto.charCodeAt(x) + 65 - n) % 26 + 65;
+        resultado = resultado+String.fromCharCode(code);
+        
+    }
+    document.getElementById("resultado").value=resultado;
+}
 };
