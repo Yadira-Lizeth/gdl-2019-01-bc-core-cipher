27d1c5a568919d953b25649b7b0bddd9c1ad8655
Yadira-Lizeth <sainz_06@outlook.com>
2019-01-31 11:18:36 -0600

    Agregue la función decode a index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 
-function myFunction() {
+function encode() {
     //var n = parseInt(prompt ("introduce el desplzamiento"));
     var resultado ="";
     var n = parseInt(document.getElementById("numero").value);
@@ -11,6 +11,22 @@ function myFunction() {
     for (var x = 0; x < texto.length; x++) {
         var code = (texto.charCodeAt(x) - 65 + n) % 26 + 65;
         resultado = resultado+String.fromCharCode(code);
+        
+    }
+    document.getElementById("resultado").value=resultado;
+}
+
+function decode() {
+    //var n = parseInt(prompt ("introduce el desplzamiento"));
+    var resultado ="";
+    var n = parseInt(document.getElementById("numero").value);
+    //var texto = prompt ("introduce tu texto");
+    var texto = document.getElementById("contenido").value;
+    texto = String(texto).toUpperCase();
+    for (var x = 0; x < texto.length; x++) {
+        var code = (texto.charCodeAt(x) + 65 - n) % 26 + 65;
+        resultado = resultado+String.fromCharCode(code);
+        
     }
     document.getElementById("resultado").value=resultado;
 }
